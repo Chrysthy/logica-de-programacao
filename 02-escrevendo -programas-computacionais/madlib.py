f88d9a4f80336d4c662a1e0b1ecf287ad77d4d99
Chrystine Martins <chrystine.martins@outlook.com>
2025-09-13 17:00:27 -0300

    Add criação do jogo Madlib

---
+++ 02-escrevendo -programas-computacionais/madlib.py
@@ -0,0 +1,37 @@
+print('Boas Vindas ao nosso jogo de MADLIB!')
+
+substantivo = input('Diga um nome: ')
+adjetivo = input('Diga um adjetivo: ')
+verbo = input('Diga um verbo: ')
+objeto = input('Diga um objeto: ')
+profissao = input('Diga uma profissão: ')
+adjetivo2 = input('Diga um adjetivo: ')
+numero = input('Diga um número: ')
+animal = input('Diga um animal: ')
+objeto2 = input('Diga um objeto: ')
+objeto3 = input('Diga um objeto: ')
+
+
+print("A Grande Aventura no Escritório de Tecnologia")
+
+print("Um dia, o " + substantivo + " estava trabalhando em um projeto super " + adjetivo + ". De repente, o computador começou a " + verbo + " sozinho!")
+
+print()
+
+print("Sem pensar duas vezes, o(a) " + substantivo + " pegou seu " + objeto + " e começou a digitar comandos como um(a) verdadeiro(a) " + profissao + ".")
+
+print()
+
+print('"Preciso consertar isso antes que o sistema fique ' + adjetivo2 + '!" exclamou o(a) ' + substantivo + '. Depois de ' + numero + ' tentativas, finalmente o computador respondeu: ')
+
+print()
+
+print('"O problema foi causado por um ' + animal + ' que estava no sistema!"')
+
+print()
+
+print('Com isso, o problema foi resolvido, e o(a) ' + substantivo + ' ganhou o prêmio de ' + objeto2 + ' mais inovador do ano.')
+
+print()
+
+print('Moral da história: sempre leve seu ' + objeto3 + ' para o escritório, porque nunca se sabe quando você vai precisar!')

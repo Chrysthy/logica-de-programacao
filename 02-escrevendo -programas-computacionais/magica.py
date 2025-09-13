9a3c0650e3a4c2ac0acd276d8aae7a423179c189
Chrystine Martins <chrystine.martins@outlook.com>
2025-09-13 16:25:37 -0300

    Add um truque de mágica em python

---
+++ 02-escrevendo -programas-computacionais/magica.py
@@ -0,0 +1,19 @@
+import os
+
+os.system('cls')
+
+print('Olhe as cartas abaixo e escolha uma:')
+print("---")
+print('Q♣️  K♦️  J♥️  Q♥️  J♣️  K♠️')
+input("Pressione Enter quando escolher...")
+
+os.system('cls')
+print('A carta que você pensou...')
+input("Pressione Enter para verificar...")
+
+print()
+print('Desapareceu!')
+print()
+
+print('J♠️  Q♦️  J♦️  Q♠️  K♥️')
+input('Pressione Enter para finalizar...')

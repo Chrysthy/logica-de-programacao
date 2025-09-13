0decdbf70eaf0be5005d22fabe13eb59f3b472c5
Chrystine Martins <chrystine.martins@outlook.com>
2025-09-13 16:39:56 -0300

    Add Variáveis e tipos de dados

---
+++ 02-escrevendo -programas-computacionais/variaveis-e-tipos-de-dados.py
@@ -0,0 +1,12 @@
+# Tipos de Dados
+# numéricos, texto, booleano (lógico)
+# algumas linguagens de programação são estaticamente tipadas
+# python é dinamicamente tipada
+# não precisamos declarar o tipo da variável
+
+x = 15
+# print(15 + "agua") # dá erro
+
+x = "Chrystine" 
+print(x)
+dir(x) # mostra os métodos disponíveis para o tipo da variável. No terminal interativo.

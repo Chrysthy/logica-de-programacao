d8cf702edc1ae3df4693d20f52908a3944059846
Chrystine Martins <chrystine.martins@outlook.com>
2025-09-13 17:31:13 -0300

    Add Estruturas Condicionais

---
+++ 03-controlando-o-fluxo-de-execucao/teste-condicional.py
@@ -0,0 +1,11 @@
+print('Bem-vindo ao DETRAN')
+print('Este é o novo teste extremamente complexo para liberar ou não a CNH')
+print()
+idade = int(input('Digite a sua idade: ')) # convertendo a string para inteiro
+
+if idade >= 18:
+    print('Você está apto a fazer o teste')
+else:
+    print('Você não está apto a fazer o teste')
+
+print('Acabou')

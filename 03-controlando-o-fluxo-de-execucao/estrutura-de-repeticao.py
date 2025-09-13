c48614091838b640748a54a28191236cfa7a3e93
Chrystine Martins <chrystine.martins@outlook.com>
2025-09-13 19:14:32 -0300

    Add while example

---
+++ 03-controlando-o-fluxo-de-execucao/estrutura-de-repeticao.py
@@ -0,0 +1,11 @@
+soma = 0
+n = 1
+
+# while n <=10:
+#     soma = soma + n
+#     n = n + 1
+#     print(f'Soma: {soma}') 
+#     print(f'n: {n}')
+#     #O f antes das aspas indica que é uma f-string (string formatada). Ela permite inserir valores de variáveis diretamente dentro da string usando {}.
+
+# print(f'A soma dos 10 primeiros números é {soma}')

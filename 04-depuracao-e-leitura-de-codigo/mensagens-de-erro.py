0c225dfa7316233225eb7a783c9d856cb61c4b47
Chrystine Martins <chrystine.martins@outlook.com>
2025-09-14 10:04:43 -0300

    Add entendendo mensagens de erro

---
+++ 04-depuracao-e-leitura-de-codigo/mensagens-de-erro.py
@@ -0,0 +1,12 @@
+# Running e o nome do arquivo = está rodando o arquivo tal
+
+# Tem vezes que a descrição do erro é confusa, mas o número da linha e o tipo de erro ajudam a entender o que está acontecendo, onde está o erro e a setinha ^ no local do erro. Ex.: de soma(a,b)
+
+# Done = terminou de rodar o arquivo
+# Done code=0 = sucesso
+# Done code=1 = erro
+
+# Tipos de erros mais comuns:
+# Erro léxico = escrever palavra errada
+# Erro sintático = esquecer de colocar algo ou colocar algo no lugar errado
+# Erro semântico = o código está correto, mas o resultado não é o esperado

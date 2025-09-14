6de34d362db1174b634d734418440a8ef07d9fe1
Chrystine Martins <chrystine.martins@outlook.com>
2025-09-14 14:48:42 -0300

    Add tipos de dados

---
+++ estruturas.py
@@ -0,0 +1,29 @@
+# Tipos primitivos de dados em Python
+
+# - int: números inteiros
+# - float: números decimais (ponto flutuante)
+# - str: cadeia de caracteres (texto)
+# - bool: valores lógicos (True ou False)
+
+
+# Operadores aritméticos:
+#+, -, * ...
+
+# Operadores lógicos:
+#and, or, not
+# True e False (sempre com a inicial maiúscula)
+
+# Operadores de strings:
+# +, *
+
+# Operadores de comparação: recebem valores de diversos tipos e retornam um bool
+# == igualdade
+# != diferença
+# > maior que
+# < menor que
+# >= maior igual a
+# <= menor igual a
+
+# // é a divisão inteira
+# % é o resto da divisão
+# ** é o operador de potência   

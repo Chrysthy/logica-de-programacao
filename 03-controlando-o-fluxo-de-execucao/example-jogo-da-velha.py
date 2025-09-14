69acd3e8fcccbd7ced86ca05785616e3a5cc1ced
Chrystine Martins <chrystine.martins@outlook.com>
2025-09-14 09:16:30 -0300

    Add lógica do ganhador e empate

--- 03-controlando-o-fluxo-de-execucao/example-jogo-da-velha.py
+++ 03-controlando-o-fluxo-de-execucao/example-jogo-da-velha.py
@@ -6,7 +6,7 @@ tabuleiro = [
 
 jogador = 'X'
 
-print(tabuleiro)
+#print(tabuleiro)
 
 def exibirTabuleiro():
     for linha in tabuleiro:
@@ -27,6 +27,58 @@ def jogada(linha, coluna):
 
     return 'O' if jogador == 'X' else 'X'
 
+
+def temGanhador():
+
+    """ Verifica as linhas """
+    for linha in range(3):
+        if (
+            tabuleiro[linha][0] != ' ' and
+            tabuleiro[linha][0] == tabuleiro[linha][1] and
+            tabuleiro[linha][0] == tabuleiro[linha][2]
+        ):
+            print(f'{tabuleiro[linha][0]} GANHOU!!!')
+            return True
+
+    """ Verifica as colunas """
+    for coluna in range(3):
+        if (
+            tabuleiro[0][coluna]!= ' ' and
+            tabuleiro[0][coluna] == tabuleiro[1][coluna] and
+            tabuleiro[0][coluna] == tabuleiro[2][coluna]
+        ):
+            print(f'{tabuleiro[0][coluna]} GANHOU!!!')
+            return True
+
+    """ Verifica as diagonais """
+    if (
+        tabuleiro[1][1] != ' ' and
+        (
+            (
+                tabuleiro[0][0] == tabuleiro[1][1] and
+                tabuleiro[0][0] == tabuleiro[2][2]
+            ) or
+            (
+                tabuleiro[0][2] == tabuleiro[1][1] and
+                tabuleiro[1][1] == tabuleiro[2][0]
+            )
+        )
+    ):
+            print(f'{tabuleiro[1][1]} GANHOU!!!')
+            return True
+
+    """ Se não teve ganhador em nenhuma forma """
+    return False
+
+def temEmpate():
+    for linha in range(3):
+        for coluna in range(3):
+            if tabuleiro[linha][coluna] == ' ':
+                return False
+    print('Acabou empatado!')
+    return True
+
+
 while True:
     print(f'Jogador da vez: {jogador}')
 
@@ -38,5 +90,8 @@ while True:
         print('Entrada inválida! Por favor, digite valores numéricos inteiros entre 0 e 2para linha e coluna.')
     except (IndexError):
         print('Entrada inválida! Por favor, digite valores numéricos inteiros entre 0 e 2para linha e coluna.')
+
+    if temGanhador() or temEmpate():
+        break
 
     exibirTabuleiro()
